ca044d99f476b9bfb1a9766d0ae173e2609d663c
ipopop <popop@ntymail.com>
2023-10-20 20:27:59 +0200

    test 2 - TDD refactor - src/tokenizer.py

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,18 +1,67 @@
 class Tokenizer:
-    def __init__(self):
-        pass
+    """A class used to tokenize text and perform related tasks."""
 
-    def tokenize(self, text):
+    @staticmethod
+    def tokenize(text):
+        """
+        Tokenizes the input text.
+
+        Parameters:
+        text (str): The text to tokenize.
+
+        Returns:
+        list: A list of tokens.
+        """
         return []
 
-    def token_frequency(self, text):
+    @staticmethod
+    def token_frequency(text):
+        """
+        Calculates the frequency of each token in the input text.
+
+        Parameters:
+        text (str): The text to analyze.
+
+        Returns:
+        dict: A dictionary where the keys are tokens and the values are their frequencies.
+        """
         return {}
 
-    def generate_word_cloud(self, text):
+    @staticmethod
+    def generate_word_cloud(text):
+        """
+        Generates a word cloud from the input text.
+
+        Parameters:
+        text (str): The text to generate a word cloud from.
+
+        Returns:
+        None
+        """
         return None
 
-    def remove_stopwords(self, text):
+    @staticmethod
+    def remove_stopwords(text):
+        """
+        Removes stopwords from the input text.
+
+        Parameters:
+        text (str): The text to remove stopwords from.
+
+        Returns:
+        str: The text with stopwords removed.
+        """
         return text
 
-    def remove_punctuation_and_numbers(self, text):
+    @staticmethod
+    def remove_punctuation_and_numbers(text):
+        """
+        Removes punctuation and numbers from the input text.
+
+        Parameters:
+        text (str): The text to remove punctuation and numbers from.
+
+        Returns:
+        str: The text with punctuation and numbers removed.
+        """
         return text
