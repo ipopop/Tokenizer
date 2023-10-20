1b9b604c26dec8e36bfb43958d0c164ca94c2b75
ipopop <popop@ntymail.com>
2023-10-20 20:23:37 +0200

    test 2 - TDD green  - add minimal src/tokenizer.py

---
+++ src/tokenizer/tokenizer.py
@@ -0,0 +1,18 @@
+class Tokenizer:
+    def __init__(self):
+        pass
+
+    def tokenize(self, text):
+        return []
+
+    def token_frequency(self, text):
+        return {}
+
+    def generate_word_cloud(self, text):
+        return None
+
+    def remove_stopwords(self, text):
+        return text
+
+    def remove_punctuation_and_numbers(self, text):
+        return text
