7550a545bd9189ac1938c679fa83667797977ed4
ipopop <popop@ntymail.com>
2023-10-20 21:18:36 +0200

    test 5 - TDD refactor src/wordcloud_generator.py

--- src/wordcloud_generator/wordcloud_generator.py
+++ src/wordcloud_generator/wordcloud_generator.py
@@ -1,6 +1,6 @@
 from wordcloud import WordCloud
 
 class WordcloudGenerator:
-    def generate_wordcloud(self, text):
-        wordcloud = WordCloud().generate(text)
+    def generate_wordcloud(self, text: str, max_words: int = 200, colormap: str = 'viridis') -> WordCloud:
+        wordcloud = WordCloud(max_words=max_words, colormap=colormap).generate(text)
         return wordcloud
