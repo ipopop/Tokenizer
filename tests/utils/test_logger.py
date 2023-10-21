b70bc8f9bf87cba3e0c4a3dc937b4658fcb81774
ipopop <popop@ntymail.com>
2023-10-21 16:01:30 +0200

    update test_logger.py

--- tests/utils/test_logger.py
+++ tests/utils/test_logger.py
@@ -1,39 +1,22 @@
-import unittest
 import os
+import logging
 from src.utils.logger_setup import logger_setup
+import unittest
 
 class TestLogging(unittest.TestCase):
+    def setUp(self):
+        self.log_file = 'test_log.log'
+        self.logger = logger_setup(self.log_file)
+
+    def tearDown(self):
+        # Remove the log file after the test
+        os.remove(self.log_file)
+
     def test_log_message(self):
-        # Setup
-        log_file = 'test_log.log'
-        logger = logger_setup(log_file)
+        message = 'Test log message'
+        self.logger.info(message)
 
-        # Log a message
-        logger.info('Test message')
-
-        # Check that the log file contains the message
-        with open(log_file, 'r') as file:
+        # Check if the log message is in the log file
+        with open(self.log_file, 'r') as file:
             log_content = file.read()
-        self.assertIn('Test message', log_content)
-
-        # Remove log file if it exists
-        if os.path.exists(log_file):
-            os.remove(log_file)
-
-        # Setup logger again for a new log file
-        logger = logger_setup(log_file)
-
-        # Log another message
-        logger.info('Test message 2')
-
-        # Check that the new log file contains the second message
-        with open(log_file, 'r') as file:
-            log_content = file.read()
-        self.assertIn('Test message 2', log_content)
-
-        # Remove log file if it exists
-        if os.path.exists(log_file):
-            os.remove(log_file)
-
-if __name__ == '__main__':
-    unittest.main()
+            self.assertIn(message, log_content)
