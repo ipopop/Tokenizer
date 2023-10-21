9f955eda6428d432d181777877af33147442c67f
ipopop <popop@ntymail.com>
2023-10-21 15:57:18 +0200

    update tokenizer.py

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import WordPunctTokenizer
 from src.utils.logger_setup import logger_setup
+from collections import Counter
 
 class Tokenizer:
     """A class used to tokenize text and perform related tasks."""
@@ -26,3 +27,25 @@ class Tokenizer:
         logger.info(f'Generated {len(tokens)} tokens')
 
         return tokens
+
+    @staticmethod
+    def token_frequency(tokens, log_file='logs/project_logs.log'):
+        """
+        Calculates the frequency of each token in the list of tokens.
+
+        Parameters:
+        tokens (list): The list of tokens.
+
+        Returns:
+        dict: A dictionary where the keys are tokens and the values are frequencies.
+        """
+        # Set up logger
+        logger = logger_setup(log_file)
+
+        # Calculate token frequencies
+        token_freq = Counter(tokens)
+
+        # Log the number of unique tokens
+        logger.info(f'Calculated frequencies for {len(token_freq)} unique tokens')
+
+        return token_freq
