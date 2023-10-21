a21a049e505d11c9927a4eac545d10ff705022b5
ipopop <popop@ntymail.com>
2023-10-21 16:10:08 +0200

    update test_tokenizer.py

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,6 +1,8 @@
 from nltk.tokenize import WordPunctTokenizer
 from src.utils.logger_setup import logger_setup
 from collections import Counter
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 class Tokenizer:
     """A class used to tokenize text and perform related tasks."""
@@ -49,3 +51,25 @@ class Tokenizer:
         logger.info(f'Calculated frequencies for {len(token_freq)} unique tokens')
 
         return token_freq
+
+    @staticmethod
+    def generate_word_cloud(text, log_file='logs/project_logs.log'):
+        """
+        Generates a word cloud from the input text.
+
+        Parameters:
+        text (str): The text to generate a word cloud from.
+        """
+        # Set up logger
+        logger = logger_setup(log_file)
+
+        # Generate word cloud
+        wordcloud = WordCloud().generate(text)
+
+        # Display the generated image
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.show()
+
+        # Log the generation of the word cloud
+        logger.info(f'Generated word cloud for text of length {len(text)}')
