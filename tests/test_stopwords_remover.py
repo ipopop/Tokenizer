b991137fc958c9e7ed79c1343e0ff05b8989107c
ipopop <popop@ntymail.com>
2023-10-21 15:58:51 +0200

    update test_stopwords_remover.py

--- tests/test_stopwords_remover.py
+++ tests/test_stopwords_remover.py
@@ -1,14 +1,12 @@
-import unittest
 from src.stopwords_remover.stopwords_remover import StopwordsRemover
+import unittest
 
 class TestStopwordsRemover(unittest.TestCase):
     def setUp(self):
-        self.stopwords_remover = StopwordsRemover()
+        self.remover = StopwordsRemover()
 
     def test_remove_stopwords(self):
-        text_with_stopwords = "This is a sample sentence, showing off the stop words filtration."
-        text_without_stopwords = self.stopwords_remover.remove_stopwords(text_with_stopwords)
-        self.assertEqual(text_without_stopwords, "This sample sentence , showing stop words filtration .")
-
-if __name__ == '__main__':
-    unittest.main()
+        text = "This is a test"
+        expected = "This test"
+        actual = self.remover.remove_stopwords(text)
+        self.assertEqual(expected, actual)
